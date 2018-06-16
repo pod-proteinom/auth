ee97d569f5a5799e330cd1473b646aedbf7596f1
pods13 <alta0815@yandex.ru>
2018-06-17 00:11:27 +0300

    Configure jest for e2e

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,41 @@
 import request from 'supertest';
 import { Test } from '@nestjs/testing';
-import { AppModule } from './../src/app.module';
+import { ImagesModule } from './../src/images/images.module';
+import { ConfigModule } from './../src/config/config.module';
+import { AuthenticationModule } from './../src/authentication/authentication.module';
 import { INestApplication } from '@nestjs/common';
+import { ConfigServiceToken } from '../src/config/constants';
+import { UsersModule } from '../src/users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Repository, Connection } from 'typeorm';
+import { UsersModuleProviders } from '../src/users/users.providers';
+import { User } from '../src/users/user.entity';
+import {AppModule} from '../src/app.module';
+import { TransformImageServiceToken } from '../src/images/constants';
+
+jest.mock('tinify');
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeAll(async () => {
     const moduleFixture = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [AppModule]
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
   });
 
-  it('/GET /', () => {
+  it('/GET /pages', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .get('/pages/slug/help')
+      .expect(200);
+  });
+
+  afterAll(async () => {
+    const connection = app.get(Connection);
+    await connection.close();
+    await app.close();
   });
 });
